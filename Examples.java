ec0aeb658de4ef822a22f044c5352981a60b96e8
ElliotScher <ecscher84@gmail.com>
2024-11-20 20:35:26 -0500

    all tests pass autograder.

--- Examples.java
+++ Examples.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
 
 public class Examples {
     
@@ -12,6 +17,16 @@ public class Examples {
                 new BTNode<>(2, empty, empty)),
             new BTNode<>(5,
                 empty,
+                new BTNode<>(4, empty, empty))
+        );
+
+    IBinTree<Integer> exMaxHeapInvalid =
+        new BTNode<>(7,
+            new BTNode<>(3,
+                new BTNode<>(1, empty, empty),
+                new BTNode<>(2, empty, empty)),
+            new BTNode<>(5,
+                new BTNode<>(6, empty, empty),
                 new BTNode<>(4, empty, empty))
         );
     
@@ -36,6 +51,31 @@ public class Examples {
                 new BTNode<>(2, empty, empty)),
             new BTNode<>(4, empty, empty)
         );
+
+
+    IBinTree<Integer> exMinHeap =
+        new BTNode<>(1,
+            new BTNode<>(2,
+                new BTNode<>(3, empty, empty),
+                new BTNode<>(4, empty, empty)),
+            new BTNode<>(5,
+                empty,
+                new BTNode<>(7, empty, empty))
+        );
+
+    IBinTree<Integer> exMinHeapInvalid =
+        new BTNode<>(1,
+            new BTNode<>(2,
+                new BTNode<>(3, empty, empty),
+                new BTNode<>(4, empty, empty)),
+            new BTNode<>(5,
+                empty,
+                new BTNode<>(7,
+                    new BTNode<>(5, empty, empty),
+                empty))
+        );
+
+    BST<Integer> bst = new BST<>(exMaxHeap);
     
     @Test
     public void testAdd6OK() {
@@ -53,5 +93,56 @@ public class Examples {
     public void testAdd6NotOK() {
         ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
         assertFalse(maxHValid.validAdd(exMaxHeap, 5, exMaxHeap));
+    }
+
+    @Test
+    public void testRemove5NotOK() {
+        ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
+        assertFalse(maxHValid.validRemove(exMaxHeap, 5, exMaxHeap));
+    }
+
+    @Test
+    public void testInvalidMaxHeap() {
+        ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
+        assertFalse(maxHValid.validAdd(exMaxHeap, 6, exMaxHeapInvalid));
+    }
+
+    @Test
+    public void testInvalidMinHeap() {
+        ValidatorMinHeap<Integer> minHValid = new ValidatorMinHeap<>();
+        assertFalse(minHValid.validAdd(exMinHeap, 5, exMinHeapInvalid));
+    }
+
+    @Test
+    public void testInvalidAddElementAdded() {
+        ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
+        assertFalse(maxHValid.validAdd(exMaxHeap, 9, afterAdd6));
+    }
+
+    @Test
+    public void searchNotFound() {
+        assertEquals(Optional.empty(), bst.search(10));
+    }
+
+    @Test
+    public void searchFound() {
+        assertEquals(Optional.of(7), bst.search(7));
+    }
+
+    @Test
+    public void searchFoundLeft() {
+        assertEquals(Optional.of(3), bst.search(3));
+    }
+
+    @Test
+    public void testInvalidAddBeforeInAfter() {
+        ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
+        assertFalse(maxHValid.validAdd(afterAdd6, 6, exMaxHeap));
+    }
+
+    @Test
+    public void testInvalidRemoveAfterInBefore() {
+        ValidatorMaxHeap<Integer> maxHValid = new ValidatorMaxHeap<>();
+        assertFalse(maxHValid.validRemove(afterRemove5, 5, exMaxHeap));
     }
 }
