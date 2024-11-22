244135ca0e38b3116ce34676ddb4d2c217eaae44
ElliotScher <ecscher84@gmail.com>
2024-11-22 17:54:13 -0500

    all tests pass autograder.

--- BST.java
+++ BST.java
@@ -9,12 +9,18 @@ public class BST<T extends Comparable<T>> extends BinaryTree<T>{
     }
 
     public Optional<T> search(T key) {
-        return null;
-        // TODO
+        return search(key, this.data);
     }
 
     private Optional<T> search(T key, IBinTree<T> someTree) {
-        return null;
-        // TODO
+        if (someTree.isEmpty()) {
+            return Optional.empty();
+        } else if (someTree.getRoot().compareTo(key) == 0) {
+            return Optional.of(someTree.getRoot());
+        } else if (key.compareTo(someTree.getRoot()) < 0) {
+            return search(key, someTree.getLeft());
+        } else {
+            return search(key, someTree.getRight());
+        }
     }
 }
